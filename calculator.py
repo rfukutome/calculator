6cf2418d1b874cba2892a7ec2bed91404ea8b669
Richard Fukutome <rfukutome@gmail.com>
2017-10-09 01:09:44 -0700

    First commit of an empty project

---
+++ calculator.py
@@ -0,0 +1,49 @@
+'''
+Adds the second number from the first number
+@input:
+   int a - first number
+   int b - second number
+@return: Sum of the subtraction
+@rtype: int
+'''
+def add(a, b):
+
+
+'''
+Subtracts the second number from the first number
+@input:
+   int a - first number
+   int b - second number
+@return: Remainder of the subtraction
+@rtype: int
+'''
+def subtract(a, b):
+
+'''
+Multiplies the the two numbers
+@input:
+   float a - first number
+   float b - second number
+@return: multiplication of the two numbers
+@rtype: int
+'''
+def multiply(a, b):
+
+'''
+Divides the the two numbers
+@input:
+   int a - first number
+   int b - second number
+@return: Division of the first number with the second number
+@rtype: float
+'''
+def divide(a, b):
+
+'''
+Squares the numbers
+@input:
+   float a - first number
+@return: The square of the number
+@rtype: float
+'''
+def square(a):
